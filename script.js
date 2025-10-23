051a300f3bfd49f67d839984868d3764439d5032
ANDREW NOTERMANS <98767705+andrewnotermans@users.noreply.github.com>
2025-10-23 18:12:18 +0200

    feat: add gallery section with styling and functionality

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Function to crerate and observe Intersection Observers
+  // Function to create and observe IntersectionObservers
   function createObserver(selector, observerOptions, toggleClass) {
     const items = document.querySelectorAll(selector);
     const observer = new IntersectionObserver((entries) => {
       entries.forEach(entry => {
-        if(entry.isIntersecting){
+        if (entry.isIntersecting) {
           entry.target.classList.add(toggleClass);
         } else {
           entry.target.classList.remove(toggleClass);
         }
-      })
+      });
     }, observerOptions);
 
     items.forEach(item => {
@@ -18,7 +18,8 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // Create observers for different sections
-  createObserver('#about .phrase', { root: null, threshold: 1}, 'active');
+  createObserver('#about .phrase', { root: null, threshold: 1 }, 'active');
+  createObserver('#gallery .image-box', { root: null, threshold: 1 }, 'active');
 });
 
 // Navigation ----------------------------------------
