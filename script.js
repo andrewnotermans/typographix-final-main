69be4f35b5c59ad19b8702891d898ebc28a48105
ANDREW NOTERMANS <98767705+andrewnotermans@users.noreply.github.com>
2025-10-31 16:21:29 +0100

    feat: add contact section with form and styling

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
   createObserver('#about .phrase', { root: null, threshold: 1 }, 'active');
   createObserver('#gallery .image-box', { root: null, threshold: 1 }, 'active');
   createObserver('#blog .featured-articles, #blog .article', {root: null, threshold: 0.3 }, 'fadeInUp');
+  createObserver('#contact > div', {root: null, threshold: 0.7 }, 'fadeInUp');
 });
 
 // Navigation ----------------------------------------
